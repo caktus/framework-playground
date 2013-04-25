bfdbfe7017d044f60ca23be810503a7a8a9e687c
Mark Lavin <mlavin@caktusgroup.com>
2013-04-25 16:06:46 -0400

    Adding Flask example.

---
+++ python/flask/README.rst
@@ -0,0 +1,11 @@
+Flask Example
+================================================================================
+
+This is a Flask example of the spell checker service. To run it requires::
+
+    Flask==0.9
+    pyenchant==1.6.5
+
+To run the server::
+
+    python example.py
---
+++ python/flask/example.py
@@ -0,0 +1,23 @@
+import json
+import re
+
+import enchant
+from flask import Flask, request, abort, jsonify
+
+
+app = Flask(__name__)
+
+english = enchant.Dict("en_US")
+
+
+@app.route("/check")
+def check_spelling():
+    word = request.args.get('q', None) or None
+    if word is None:
+        abort(400)
+    valid = re.match(r'[a-zA-Z]+', word)
+    return jsonify(valid=valid and english.check(word))
+
+
+if __name__ == "__main__":
+    app.run()
