bf3c993bd99491a4e01f861c77a1752d7c0027cb
mlavin <markdlavin@gmail.com>
2013-05-04 21:08:16 -0400

    Working Cowboy based REST handler.

--- erlang/cowboy/src/check_handler.erl
+++ erlang/cowboy/src/check_handler.erl
@@ -1,17 +1,40 @@
 -module(check_handler).
 
--behavior(cowboy_http_handler).
-
 -export([init/3]).
--export([handle/2]).
+-export([content_types_provided/2]).
+-export([allowed_methods/2]).
+-export([malformed_request/2]).
+-export([to_json/2]).
 -export([terminate/3]).
+-export([check_spelling/1]).
 
-init(_Transport, Req, []) ->
-    {ok, Req, undefined}.
+init(_Transport, _Req, []) ->
+    {upgrade, protocol, cowboy_rest}.
 
-handle(Req, State) ->
-    {ok, Req2} = cowboy_req:reply(200, [], <<"Hello world!">>, Req),
-    {ok, Req2, State}.
+content_types_provided(Req, State) ->
+    {[{<<"application/json">>, to_json}], Req, State}.
+
+allowed_methods(Req, State) ->
+    {[<<"GET">>], Req, State}.
+
+malformed_request(Req, State) ->
+    {Q, Req2} = cowboy_req:qs_val(<<"q">>, Req),
+    {Q == undefined, Req2, State}.
+
+to_json(Req, State) ->
+    {Q, Req2} = cowboy_req:qs_val(<<"q">>, Req),
+    Valid = check_spelling(binary_to_list(Q)),
+    Result = {[{valid, Valid}]},
+    Body = jiffy:encode(Result),
+    {Body, Req2, State}.
 
 terminate(_Reason, _Req, _State) ->
     ok.
+
+check_spelling(Word) ->
+    case re:run(Word, "[a-zA-Z]+") of
+        {match, _} -> Command = string:join(["echo",  Word , "|", "enchant", "-a", "-l"], " "),
+            Result = lists:nth(2, string:tokens(os:cmd(Command), "\n")),
+            lists:sublist(Result, 1) == "*";
+        nomatch -> false
+    end.
